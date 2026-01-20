6d3a172cacc7ca2b930fc3cab1e394a21d3fb2c5
Nabil123-ops <dahdouhn74@gmail.com>
2026-01-20 09:23:23 +0200

    Update image optimization settings in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Keep TypeScript ignoring build errors
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  // Image configuration
   images: {
-    unoptimized: true,
+    unoptimized: false, // set to false so Next.js optimizes images
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image2url.com",
+        port: "", // leave empty
+        pathname: "/r2/default/images/**", // allow all images in that path
+      },
+    ],
   },
- 
 }
 
 export default nextConfig
